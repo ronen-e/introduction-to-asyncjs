98ecfa8ae6ea916f327e6c5c25af12fbdf475fc7
ronen-e <alter.ronen@gmail.com>
2020-01-11 23:57:48 +0200

    Add examples for async fetch data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <button @click="fetchXHR()">fetchXHR</button>
+      <button @click="fetchPromise()">fetchPromise</button>
+      <button @click="fetchAsyncAwait()">fetchAsyncAwait</button>
+      <pre style="text-align:left">{{response}}</pre>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
+  data() {
+    return {
+      response: '',
+    };
+  },
   components: {
-    HelloWorld,
+    // HelloWorld,
+  },
+  methods: {
+    fetchXHR() {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', '/data.json');
+      xhr.send();
+      xhr.onload = (event) => {
+        this.response = event.target.response;
+      };
+    },
+    fetchPromise() {
+      window.fetch('/data.json')
+        .then(res => res.json())
+        .then((res) => { this.response = JSON.stringify(res, undefined, 4); });
+    },
+    async fetchAsyncAwait() {
+      const response = await window.fetch('/data.json');
+      const json = await response.json();
+      this.response = json;
+    },
   },
 };
 </script>
+
+<style lang="scss">
+  button {
+    padding: 10px;
+    margin: 4px;
+    font-size: 1em;
+    cursor: pointer;
+    font-weight: bold;
+  }
+</style>
